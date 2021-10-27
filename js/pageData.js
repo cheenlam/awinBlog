133715322762e340e61f14e9abb8e1a63f17abad
cheenlam <56752830+cheenlam@users.noreply.github.com>
2021-10-27 14:35:05 +0800

    Add files via upload

---
+++ js/pageData.js
@@ -0,0 +1,130 @@
+let hdMenu = [
+    { href: '#/home', title: 'Thúy Diễm' },
+    { href: '#/about', title: 'Liên quan' }
+]
+
+let homeList = [{
+        title: '',
+        title2: '',
+        article: '',
+        img: 'images/article/pic_01.jpg',
+        alt: 'rang wed này chỉ cần giới thiệu người đến chơi là mỗi tháng đều có tiền thưởng giới thiệu.',
+        note: [
+            'Nhà cái AWin7 hoạt động khá lâu trên thị trường cá cược trực tuyến. Nhưng trong vài năm trở lại đây, nhà cái này mới được nhiều người chơi Việt biết tới hơn.',
+        ]
+    }, {
+        title: 'Một số thông tin về nhà cái AWin77.',
+        title2: '',
+        article: 'AWin77 bắt đầu hoạt động trên thị trường cá cược trực tuyến từ năm 2007. Tới nay, nhà cái AWin77 đã trở thành một trong những nền tảng cá cược giải trí trực tuyến lớn bậc nhất tại khu vực châu Á. Nhà cái được ủy quyền và chịu sự giám sát chặt chẽ bởi First Cagayan Leisure and Resort Corporation của Philippines.',
+        img: 'images/article/pic_02.jpg',
+        alt: '',
+        note: [
+            'Trong suốt quá trình hoạt động, nhà cái luôn đảm bảo tuân theo đúng mọi hoạt động của cơ quan quản lý nhà nước Philippines. Để mang tới cho khách hàng chất lượng dịch vụ tốt nhất nhà cái đã liên kết với nhiều nền tảng cá cược quốc tế nổi tiếng. Phục vụ khách hàng trên 2 nền tảng trò chơi cá cược lớn gồm AG và allwin. Tới năm 2013, nhà cái chính thức bắt đầu hợp tác chuyên sâu với allwin.',
+        ]
+    },
+    {
+        title: 'Về vấn đề bảo mật của Awin77.',
+        title2: '',
+        article: 'AWin77 được đánh giá là công ty cung cấp dịch vụ giải trí online chuyên nghiệp quốc tế. Nhà cái đầu tư sử dụng một hệ thống tổ chức toàn diện và hoàn chỉnh. Không chỉ là sự hỗ trợ của máy móc, công nghệ, trang web cá cược còn có đội ngũ nhân viên, các chuyên gia giàu kinh nghiệm.',
+        img: 'images/article/pic_03.jpg',
+        alt: '',
+        note: [
+            'Tất cả các tựa game đều có thể trải xuống một cách dễ dàng, nhanh chóng, chắc chắn sẽ không làm bạn phải mất quá nhiều thời gian. Giao diện của website được thiết kế đơn giản, các hạng mục rõ ràng nên anh em mới tạo tài khoản cũng dễ dàng tiếp cận.',
+
+            'Đặc biệt hơn khi nhà cái awin77 chợp tác với IOVATION – hệ thống kiểm tra an ninh mạng hàng đầu của thế giới. Nhờ đó là người chơi được hỗ trợ kỹ thuật tốt nhất với những dịch vụ giải trí chất lượng tốt và mượt mà nhất. Nhà cái quy định mỗi một khách hàng chỉ được sử dụng một tài khoản chơi game.',
+
+            'AWin77.com trang bị đường truyền được mã hóa SSL 128 bit. Cùng với đó là mã hóa MH5 cho mật khẩu nhằm đảm bảo tính bảo mật của các trò chơi. Nhà cái có đội ngũ nhân viên riêng chuyên kiểm tra, giám sát nền tảng 24/24. Mọi thông tin, giao dịch của khách hàng không được phép chia sẻ hay bán cho bên thứ ba nhằm chuộc lợi cá nhân. Như vậy về tính bảo mật và công bằng tại nhà cái anh em hoàn toàn có thể an tâm tuyệt đối.'
+        ]
+    },
+    {
+        title: 'Nạp rút tiền nhanh chóng.',
+        title2: '',
+        article: '',
+        img: 'images/article/pic_04.jpg',
+        alt: '',
+        note: [
+            'Giao dịch nạp/rút tiền chậm chạp, mất nhiều thời gian gây ra không ít hoang mang và bực tức cho người chơi. Tuy nhiên tại Awin77 điều này không bao giờ xảy ra. Nhà cái cung cấp dịch vụ chuyển rút tiền vô cùng nhanh chóng, an toàn. Anh em có thể tự do nạp hay rút tiền 24/7, tốc độ siêu nhanh chỉ chưa đầy 3 phút.',
+        ]
+    },
+    {
+        title: 'Dịch vụ chăm sóc khách hàng chu đáo.',
+        title2: '',
+        article: 'Kể từ khi thành lập, nhà cái AWin77 luôn đặt ra chính sách tuyển chọn và đào tạo nhân viên cực kỳ nghiêm ngặt. Hàng năm, nhà cái tổ chức nhiều chương trình đào tạo để bổ sung nhân viên xuất sắc chuyên nghiệp. Đồng thời nâng cao chất lượng của toàn bộ đội ngũ nhân viên.',
+        img: 'images/article/pic_05.jpg',
+        alt: '',
+        note: [
+            'Nhân viên chăm sóc khách hàng không chỉ có chuyên môn mà còn phải có đạo đức. Họ có trách nhiệm trong công việc, sự tận tâm và chu đáo. Awin77 hứa hẹn cung cấp cho người chơi các dịch vụ VIP chuyên nghiệp và chu đáo 24/24.',
+        ]
+    },
+    {
+        title: 'Những dịch vụ cá cược trực tuyến nhà cái Awin77 đang cung cấp.',
+        title2: '',
+        article: 'Là một trong những nhà cái cá cược lâu năm, Awin77 tạo dựng được một sân chơi vô cùng đa dạng, phong phú với nhiều thể loại khác nhau. Trong đó có một số loại hình giải trí quen thuộc như.',
+        title2: 'Xổ số',
+        img: 'images/article/pic_06.jpg',
+        alt: '',
+        note: [
+            'Xổ số online của nhà cái Awin77 khác biệt hoàn toàn với hình thức xổ số truyền thống. Tại đây có rất nhiều cách cược, tỷ lệ trả thưởng khác nhau để anh em lựa chọn. Anh em có thể tham gia đặt cược ở xổ số 2D và 3D, 4D và 5D, số đảo, cược xiên, bao lô 7,… Mỗi sảnh chơi nhà cái đã cập nhật 2 phiên bản gồm phiên bản cổ điển và phiên bản tốc độ.',
+        ]
+    },
+    {
+        title: 'Sòng bài',
+        title2: '',
+        article: '',
+        img: 'images/article/pic_07.jpg',
+        alt: '',
+        note: [
+            'Sòng bài của Awin77 có 3 sảnh chơi để anh em thỏa sức lựa chọn, trong đó có.',
+
+            'AG Video: Với các game bài quen thuộc nhưng chưa bao giờ ngừng hấp dẫn như ba cây, rồng hổ, baccarat, sicbo…',
+
+            'WM video: Đây là sảnh chơi được yêu thích nhất bởi giao diện ấn tượng, bố cục được sắp xếp rõ ràng. Các tựa game có mặt ở đây được phân chia thành từng mục để người chơi dễ theo dõi và lựa chọn.',
+
+            'AE Sexy: Tại đây anh em có thể tham gia cá cược và trò chuyện cùng những cô nàng Dealer xinh đẹp, nóng bỏng.'
+        ]
+    },
+    {
+        title: 'Slot game',
+        title2: '',
+        article: 'Hay còn được gọi với cái tên quen thuộc là quay hũ, nổ hũ. Sảnh game đa dạng với nhiều phiên bản khác nhau. Tuy nhiên về cơ bản người chơi đều cần phải tuân thủ theo một quy tắc đó là làm cho các biểu tượng trùng lặp nhau theo hàng ngang – dọc hoặc chéo. Nếu “nổ hũ”, anh em sẽ có cơ hội ẵm về quỹ thưởng cực kỳ lớn lên tới hàng trăm triệu.',
+        img: 'images/article/pic_08.jpg',
+        alt: '',
+        note: [
+            'Hiện nhà cái Awin77 đang hợp tác với khá nhiều nhà cung cấp slot games nổi tiếng và uy tín như MG điện tử, JDB điện tử,….Mỗi sảnh chơi lại có tới hàng chục game nhỏ để anh em khám phá.',
+        ]
+    },
+    {
+        title: 'My cờ vua',
+        title2: '',
+        article: '',
+        img: 'images/article/pic_09.jpg',
+        alt: '',
+        note: [
+            'Nhiều anh em có vẻ thấy lạ với cái tên My cờ vua. Nhưng thực chất đây là một dòng game đánh bài dưới dạng đồ họa. mọi thanh của trò chơi được thiết kế sống động, hình ảnh đẹp mắt hứa hẹn mang tới một bộ môn giải trí lý tưởng cho anh em. Đặc biệt hơn, anh em có thể cùng lúc tham gia My cờ vua với nhiều game thủ trực tuyến khác. Nếu bạn còn “gà mờ” hay đã thua quá nhiều, hãy đặt cược theo người có tỷ lệ thắng cao.',
+        ]
+    },
+    {
+        title: 'Thể thao',
+        title2: '',
+        article: '',
+        img: 'images/article/pic_10.jpg',
+        alt: '',
+        note: [
+            'Hiện tại nhà cái AWin77 có 2 sảnh dành cho fan của thể thao như.',
+
+            'Sabah thể thao: Nơi cung cấp rất nhiều tỷ lệ kèo cược cho các bộ môn thể thao khác nhau như bóng chày, bóng đá, quần vợt, khúc côn cầu trên băng, bóng chuyền…',
+
+            'SB0: Sảnh thể thao này anh em có thể tham gia vào nhiều bộ môn khác nhau. Ngoài những trò quen thuộc, SB0 của AWin77 còn có thêm những lựa chọn “ảo” như bóng rổ ảo, bóng đá ảo,… Tựa game đều được thiết kế theo mô hình đồ họa vô cùng độc đáo. Đây là điểm dừng chân lý tưởng cho anh em chưa có nhiều kinh nghiệm.'
+        ]
+    },
+    {
+        title: 'Bắn cá',
+        title2: '',
+        article: '',
+        img: 'images/article/pic_11.jpg',
+        alt: '',
+        note: [
+            'Trò chơi bắn cá được Awin77 đầu tư khá lớn với đồ họa bắt mắt, âm thanh sống động mang tới cho người chơi những trải nghiệm vô cùng chân thực. Đặc biệt hơn, nhà cái còn cung cấp nhiều phiên bản khác nhau như ngũ rồng bắn cá, vua rồng câu cá, câu cá bách trúng, bàn đơn, bắn cá kho báu,….Mỗi phiên bản lại có điểm hấp dẫn riêng với tỷ lệ trả thưởng khác nhau. Đây thực sự là trò chơi giải trí lý tưởng mà anh em không nên bỏ qua.',
+        ]
+    },
+]
